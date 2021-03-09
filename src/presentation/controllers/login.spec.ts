fb31bad5a063ccda0a4afb868a9ebcb53d00ad7c
wasmenezes <was288519@gmail.com>
2021-03-09 06:37:58 -0300

    test: ensure return an error if no password is provided

--- src/presentation/controllers/login.spec.ts
+++ src/presentation/controllers/login.spec.ts
@@ -5,8 +5,19 @@ describe('Login Controller', () => {
     const sut = new LoginController()
     const httpRequest = {
       body: {
-        email: 'any_email@mail.com',
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
       }
     }
     const httpResponse = sut.handle(httpRequest)
