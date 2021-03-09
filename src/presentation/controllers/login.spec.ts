85c9a47babd8993e8dda9079179447faffb5a5dd
wasmenezes <was288519@gmail.com>
2021-03-09 06:27:55 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login.spec.ts
@@ -0,0 +1,15 @@
+import { LoginController } from './login'
+
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
