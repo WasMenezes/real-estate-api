8a5ade064fb1596f67f499657357acc92f6e474b
wasmenezes <was288519@gmail.com>
2021-03-19 06:09:31 -0300

    test: ensure AccountMongoRepository return an account if loadByEmail success

---
+++ tests/infra/db/mongodb/account-mongo-repository.spec.ts
@@ -0,0 +1,41 @@
+import { AccountMongoRepository } from '@/infra/db/mongodb/account-mongo-repository'
+import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+
+import { Collection } from 'mongodb'
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
+let accountCollection: Collection
+describe('AccountMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('loadByEmail()', () => {
+    test('should return an account on loadByEmail sucess', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+      const account = await sut.loadByEmail('any_email@mail.com')
+
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.password).toBe('any_password')
+    })
+  })
+})
