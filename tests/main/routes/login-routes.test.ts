34e969fa4c41809a2ae6fb6733fe6552c598c49d
wasmenezes <was288519@gmail.com>
2021-03-20 09:42:40 -0300

    refactor: connect to mongodb before integration tests

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,7 +1,22 @@
 import request from 'supertest'
 import app from '@/main/config/app'
+import { Collection } from 'mongodb'
+import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
 
 describe('Login Routes', () => {
+  let accountCollection: Collection
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   test('Should return an account name and accessToken on success', async () => {
     await request(app)
       .post('/api/login')
