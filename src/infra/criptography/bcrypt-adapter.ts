874ae6f3d3e1e481036ecfc918a31f00e5a3acae
wasmenezes <was288519@gmail.com>
2021-03-21 21:30:58 -0300

    feat: ensure BcryptAdapter calls compare with correct values

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,11 @@
+import { HashComparer } from '@/data/protocols/criptography/hash-comparer'
+import bcrypt from 'bcrypt'
+export class BcryptAdapter implements HashComparer {
+  constructor (
+    private readonly salt: number
+  ) { }
+
+  async compare (plaitext: string, digest: string): Promise<boolean> {
+    return await bcrypt.compare(plaitext, digest)
+  }
+}
