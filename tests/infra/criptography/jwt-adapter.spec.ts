6c1982e20905bf6d728315e714149878222af95f
wasmenezes <was288519@gmail.com>
2021-03-22 20:07:59 -0300

    test: ensure JwtAdapter trhwos if encrypt throws

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -13,10 +13,19 @@ const makeSut = (): JwtAdapter => {
 }
 
 describe('Jwt Adapter', () => {
-  test('Should JwtAdapter calls encrypt with correct values', async () => {
-    const sut = makeSut()
-    const signSpy = jest.spyOn(jwt, 'sign')
-    await sut.encrypt('any_value')
-    expect(signSpy).toBeCalledWith({ id: 'any_value' }, secret)
+  describe('encrypt()', () => {
+    test('Should JwtAdapter calls encrypt with correct values', async () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt('any_value')
+      expect(signSpy).toBeCalledWith({ id: 'any_value' }, secret)
+    })
+
+    test('Should JwtAdapter throws if encrypt throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.encrypt('any_value')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
