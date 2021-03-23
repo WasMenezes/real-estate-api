2f2c5e0c57fb102b06b723d9871f0bc817865101
wasmenezes <was288519@gmail.com>
2021-03-23 20:04:24 -0300

    feat: ensure DbAuthentication call updateAccessToken with correct values

--- src/data/usecases/db-authentication.ts
+++ src/data/usecases/db-authentication.ts
@@ -2,12 +2,14 @@ import { Authentication } from '@/domain/usecases'
 import { Encrypter } from '../protocols/criptography/encrypter'
 import { HashComparer } from '../protocols/criptography/hash-comparer'
 import { LoadAccountByEmailRepository } from '../protocols/db/account/load-account-by-email-repository'
+import { UpdateAccessToken } from '../protocols/db/account/update-access-token'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly updateAccessToken: UpdateAccessToken
   ) { }
 
   async auth (authenticationParams: Authentication.Params): Promise<Authentication.Result> {
@@ -15,7 +17,8 @@ export class DbAuthentication implements Authentication {
     if (account) {
       const isValid = await this.hashComparer.compare(authenticationParams.password, account.password)
       if (isValid) {
-        await this.encrypter.encrypt(account.id)
+        const accessToken = await this.encrypter.encrypt(account.id)
+        await this.updateAccessToken.updateAccessToken(account.id, accessToken)
       }
     }
     return null
