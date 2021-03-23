05e8e238cbb410725b293240fa34be10e84a064a
wasmenezes <was288519@gmail.com>
2021-03-22 21:36:19 -0300

    feat: ensure BcryptAdapter calls hash with correct values

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,10 +1,15 @@
+import { Encrypter } from '@/data/protocols/criptography/encrypter'
 import { HashComparer } from '@/data/protocols/criptography/hash-comparer'
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements HashComparer {
+export class BcryptAdapter implements HashComparer, Encrypter {
   constructor (
     private readonly salt: number
   ) { }
+
+  async encrypt (plaintext: string): Promise<string> {
+    return await bcrypt.hash(plaintext, this.salt)
+  }
 
   async compare (plaitext: string, digest: string): Promise<boolean> {
     return await bcrypt.compare(plaitext, digest)
