ea08165848b98bd7f32501e07377d5bbdbf957d5
wasmenezes <was288519@gmail.com>
2021-03-24 06:53:51 -0300

    test: ensure EmailValidation throws if EmailValidator throws

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -33,18 +33,26 @@ describe('EmailValidation', () => {
     expect(emailValidatorSpy).toBeCalledWith('valid_email@mail.com')
   })
 
-  test('should return true if a valid email is provided', () => {
+  test('should return null if a valid email is provided', () => {
     const { sut } = makeSut()
     const isValid = sut.validate({ email: 'valid_email@mail.com' })
     expect(isValid).toBeFalsy()
   })
 
-  test('should return InvalidParamError if validate fail', () => {
+  test('should return InvalidParamError if validate fails', () => {
     const { sut } = makeSut()
     jest.spyOn(sut, 'validate').mockImplementationOnce(() => {
       return new InvalidParamError('email')
     })
     const isValid = sut.validate('invalid_email@mail.com')
     expect(isValid).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should throw if EmailValidator throws', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    expect(sut.validate).toThrow()
   })
 })
