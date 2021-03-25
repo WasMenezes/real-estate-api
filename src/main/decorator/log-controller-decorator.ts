9cc388f4b2ede24629818688dd41cf0590c3ba15
wasmenezes <was288519@gmail.com>
2021-03-24 21:13:41 -0300

    feat: should controller call handle

---
+++ src/main/decorator/log-controller-decorator.ts
@@ -0,0 +1,13 @@
+import { LogErrorRepository } from '@/data/protocols/db/log/log-error-repository'
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+
+export class LogControllerDecorator implements Controller {
+  constructor (
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) { }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return await this.controller.handle(httpRequest)
+  }
+}
