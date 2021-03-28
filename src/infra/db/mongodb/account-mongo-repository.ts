a14f6dc12b18cf9cc814d59e825486068c69877c
wasmenezes <was288519@gmail.com>
2021-03-28 16:02:13 -0300

    feat: ensure AccountMongoRepository return an account on loadByAccount without role

--- src/infra/db/mongodb/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-mongo-repository.ts
@@ -1,8 +1,9 @@
 import { LoadAccountByEmailRepository } from '@/data/protocols/db/account/load-account-by-email-repository'
+import { LoadAccountByTokenRepository } from '@/data/protocols/db/account/load-account-by-token-repository'
 import { UpdateAccessToken } from '@/data/protocols/db/account/update-access-token'
 import { MongoHelper } from './mongo-helper'
 
-export class AccountMongoRepository implements LoadAccountByEmailRepository, UpdateAccessToken {
+export class AccountMongoRepository implements LoadAccountByEmailRepository, UpdateAccessToken, LoadAccountByTokenRepository {
   async loadByEmail (email: string): Promise<LoadAccountByEmailRepository.Result> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({ email })
@@ -18,5 +19,22 @@ export class AccountMongoRepository implements LoadAccountByEmailRepository, Upd
         accessToken: token
       }
     })
+  }
+
+  async loadByToken (token: string, role?: string): Promise<LoadAccountByTokenRepository.Result> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({
+      accessToken: token,
+      $or: [{
+        role
+      }, {
+        role: 'admin'
+      }]
+    }, {
+      projection: {
+        _id: 1
+      }
+    })
+    return account && MongoHelper.map(account)
   }
 }
