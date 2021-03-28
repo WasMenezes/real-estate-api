a0726a8ff7b7cbc034d3e8b451703787c21a5b04
wasmenezes <was288519@gmail.com>
2021-03-28 14:47:12 -0300

    feat: implents decrypt in JwtAdapter

--- src/infra/criptography/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter.ts
@@ -1,9 +1,14 @@
-import { Encrypter } from '@/data/protocols/criptography/encrypter'
+import { Decrypter, Encrypter } from '@/data/protocols/criptography'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
+
   async encrypt (plaintext: string): Promise<string> {
     return await jwt.sign({ id: plaintext }, this.secret)
+  }
+
+  async decrypt (cipherText: string): Promise<string> {
+    return await jwt.verify(cipherText, this.secret) as any
   }
 }
