28b8b3dbf53c84a33cb7b2ddaa87c7801fbdf0c9
wasmenezes <was288519@gmail.com>
2021-04-14 06:36:26 -0300

    test: ensure return 200 if any property are found

--- tests/main/routes/load-properties-routes.test.ts
+++ tests/main/routes/load-properties-routes.test.ts
@@ -2,8 +2,67 @@ import request from 'supertest'
 import app from '@/main/config/app'
 import { Collection } from 'mongodb'
 import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+import { PropertyModel, PropertyResidentialCommercialModel } from '@/domain/models/property-model'
 
 let propertyCollection: Collection
+
+const makeFakeProperty = (): PropertyModel => ({
+  id: 'any_value',
+  title: 'any_title',
+  description: 'any_description',
+  rent: true,
+  rentPrice: 999.99,
+  sale: true,
+  salePrice: 999999.99,
+  address: {
+    zipcode: 'any_zipcode',
+    street: 'any_street',
+    number: 999,
+    complement: 'any_complement',
+    neighborhood: 'any_neighborhood',
+    state: 'any_state',
+    city: 'any_city'
+  },
+  currentTributePaid: true,
+  tributeBelongsOwner: true,
+  tribute: 999.99,
+  condominium: 999.99,
+  areaTotal: 999,
+  areaUtil: 999,
+  deed: true,
+  createdAt: new Date(),
+  updated: new Date(),
+  type: {
+    id: 1,
+    description: 'any_description'
+  }
+})
+
+const makeFakePropertyResidentialCommercialModel = (): PropertyResidentialCommercialModel => ({
+  ...makeFakeProperty(),
+  registeredHousePlan: true,
+  propertyAge: 9,
+  suites: 9,
+  bathrooms: 9,
+  rooms: 9,
+  garage: 9,
+  garageCovered: 2,
+  airConditioner: true,
+  bar: true,
+  library: true,
+  barbecueGrill: true,
+  americanKitchen: true,
+  fittedKitchen: true,
+  pantry: true,
+  edicule: true,
+  office: true,
+  bathtub: true,
+  fireplace: true,
+  lavatory: true,
+  gurnished: true,
+  pool: true,
+  steamRoom: true
+})
 
 describe('AddProperty Routes', () => {
   beforeAll(async () => {
@@ -20,11 +79,21 @@ describe('AddProperty Routes', () => {
   })
 
   describe('GET /properties', () => {
-    test('Should return no content if none property are find', async () => {
+    test('Should return 204 if none property are find', async () => {
       await request(app)
         .get('/api/properties')
         .send({})
         .expect(204)
+    })
+
+    test('Should return 200 if any property are found', async () => {
+      await propertyCollection.insertOne(makeFakeProperty())
+      await propertyCollection.insertOne(makeFakePropertyResidentialCommercialModel())
+
+      await request(app)
+        .get('/api/properties')
+        .send({})
+        .expect(200)
     })
   })
 })
