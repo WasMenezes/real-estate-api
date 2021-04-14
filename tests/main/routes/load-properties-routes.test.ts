1e1dfe513d9d0df93e20b10d90100d7461eeb97f
wasmenezes <was288519@gmail.com>
2021-04-13 21:47:43 -0300

    test: ensure return no content if none property are find

---
+++ tests/main/routes/load-properties-routes.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+import { Collection } from 'mongodb'
+import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+
+let propertyCollection: Collection
+
+describe('AddProperty Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    propertyCollection = await MongoHelper.getCollection('property')
+    await propertyCollection.deleteMany({})
+  })
+
+  describe('GET /properties', () => {
+    test('Should return no content if none property are find', async () => {
+      await request(app)
+        .get('/api/properties')
+        .send({ rent: true })
+        .expect(204)
+    })
+  })
+})
